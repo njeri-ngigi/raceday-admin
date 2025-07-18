9d7af6f2fbd07f3c2c95f83d580a819560af3984
Shalon N. Ngigi <njery.ngigi@gmail.com>
2025-07-19 02:15:41 +0300

    update auth styling

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -31,35 +31,37 @@ export default function Auth() {
   }, [isAuthenticated, navigate]);
 
   return (
-    <div className="container">
-      <h2>RACE DAY ADMIN</h2>
-      <form onSubmit={handleLogin}>
-        <div className="errorContainer">
-          {/* TODO find a way to circumvent this ts error */}
-          {error?.data?.message && (
-            <p className="error">Error: {error?.data?.message}</p>
-          )}
-        </div>
-        <input
-          onChange={handleChange}
-          type="email"
-          name="email"
-          placeholder="Email"
-          title="Email"
-          required
-        />
-        <input
-          onChange={handleChange}
-          type="password"
-          name="password"
-          placeholder="Password"
-          title="Password"
-          required
-        />
-        <button type="submit" disabled={isLoading}>
-          LOGIN
-        </button>
-      </form>
+    <div className="login-wrapper">
+      <div className="login-container">
+        <h2>RACE DAY ADMIN</h2>
+        <form onSubmit={handleLogin} className="loginForm">
+          <div className="errorContainer">
+            {/* TODO find a way to circumvent this ts error */}
+            {error?.data?.message && (
+              <p className="error">Error: {error?.data?.message}</p>
+            )}
+          </div>
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            placeholder="Email"
+            title="Email"
+            required
+          />
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            placeholder="Password"
+            title="Password"
+            required
+          />
+          <button type="submit" disabled={isLoading}>
+            LOGIN
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
